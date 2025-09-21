83b6d7816d609ff7c5a9fe688ae10a45c0c6786d
Mintsuki <mintsuki@protonmail.com>
2025-09-21 09:20:45 +0200

    misc: Gate sys/idt.h include in entry.s2.c behind x86 check

--- common/entry.s2.c
+++ common/entry.s2.c
@@ -21,7 +21,9 @@
 #include <pxe/pxe.h>
 #include <pxe/tftp.h>
 #include <drivers/disk.h>
-#include <sys/idt.h>
+#if defined (__x86_64__) || defined (__i386__)
+#  include <sys/idt.h>
+#endif
 #include <sys/cpu.h>
 
 struct volume *boot_volume;
