7af48b5b352899adcdbda6e25cb24ef2a61e2e00
Mintsuki <mintsuki@protonmail.com>
2026-02-08 08:19:42 +0100

    host/limine: Fix IS_WINDOWS preprocessor precedence to correctly exclude Cygwin

--- host/limine.c
+++ host/limine.c
@@ -1,5 +1,5 @@
 #undef IS_WINDOWS
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#if (defined(WIN32) || defined(_WIN32) || defined(__WIN32)) && !defined(__CYGWIN__)
 #define IS_WINDOWS 1
 #endif
 
