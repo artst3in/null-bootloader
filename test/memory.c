5911c7cf723fdfbbe5420dbe40be3a34cfa0fcc1
Mintsuki <mintsuki@protonmail.com>
2026-02-15 22:04:25 +0100

    lib/memory: Use uintptr_t for pointer comparisons in memmove

--- test/memory.c
+++ test/memory.c
@@ -26,11 +26,11 @@ void *memmove(void *dest, const void *src, size_t n) {
     uint8_t *pdest = dest;
     const uint8_t *psrc = src;
 
-    if (src > dest) {
+    if ((uintptr_t)src > (uintptr_t)dest) {
         for (size_t i = 0; i < n; i++) {
             pdest[i] = psrc[i];
         }
-    } else if (src < dest) {
+    } else if ((uintptr_t)src < (uintptr_t)dest) {
         for (size_t i = n; i > 0; i--) {
             pdest[i-1] = psrc[i-1];
         }
