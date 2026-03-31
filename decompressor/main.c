b0569fed9520fe9cf3beae22b5c7231bf89ecc85
Mintsuki <mintsuki@protonmail.com>
2026-03-31 14:08:53 +0200

    decompressor: Re-enable tinf bounds checks, CRC32, and length validation

--- decompressor/main.c
+++ decompressor/main.c
@@ -5,9 +5,20 @@
 
 noreturn void entry(uint8_t *compressed_stage2, size_t stage2_size, uint8_t boot_drive, int pxe) {
     // The decompressor should decompress compressed_stage2 to address 0xf000.
+    // The output buffer extends up to 0x70000 where the decompressor itself lives.
     uint8_t *dest = (uint8_t *)0xf000;
+    unsigned int destLen = 0x70000 - 0xf000;
 
-    tinf_gzip_uncompress(dest, compressed_stage2, stage2_size);
+    if (tinf_gzip_uncompress(dest, &destLen, compressed_stage2, stage2_size) != 0) {
+        const char *msg = "Limine decomp error";
+        volatile uint16_t *vga = (volatile uint16_t *)0xB8000;
+        for (size_t i = 0; msg[i]; i++) {
+            vga[i] = 0x4F00 | (uint8_t)msg[i];
+        }
+        for (;;) {
+            asm volatile ("cli; hlt");
+        }
+    }
 
     asm volatile (
         "movl $0xf000, %%esp\n\t"
